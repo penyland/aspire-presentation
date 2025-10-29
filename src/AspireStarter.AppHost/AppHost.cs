89f20733faf4fbdb4ed19f2322dc77e7955bcd4a
Peter Nylander <peter.nylander@gmail.com>
2025-10-29 16:00:10 +0100

    Add AspireStarter.Web to AppHost

--- src/AspireStarter.AppHost/AppHost.cs
+++ src/AspireStarter.AppHost/AppHost.cs
@@ -2,4 +2,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var apiService = builder.AddProject<Projects.AspireStarter_ApiService>("apiService");
 
+builder.AddProject<Projects.AspireStarter_Web>("blazorWebFrontend")
+    .WithReference(apiService);
+
 builder.Build().Run();
